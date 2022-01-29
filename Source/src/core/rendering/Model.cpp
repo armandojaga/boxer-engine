04dd3d4be7177c53e66a77e0ba30feb7b231b692
Alvaro Soppelsa <alvaro.soppelsa1@hp.com>
2022-01-29 11:16:08 +0100

    Import model from data file

--- Source/src/core/rendering/Model.cpp
+++ Source/src/core/rendering/Model.cpp
@@ -1,187 +1,64 @@
 #include "Model.h"
-#include "Application.h"
-#include "modules/ModuleTexture.h"
-
 #include "Globals.h"
-#include "Math/float3.h"
 
-Model::Model(const char* file)
-    : position(0.0f, 0.0f, 0.0f)
+#include <yaml-cpp/yaml.h>
+
+Model::Model(const char* model_name)
 {
-    Load(file);
+    Load(model_name);
 }
 
-/*
- * OTHER ASSIMP POSTPROCESS USEFULL OPTIONS ARE:
- * aiProcess_GenNormals
- * aiProcess_SplitLargeMeshes
- * aiProcess_OptimizeMeshes
-*/
-void Model::Load(const char* file)
+void Model::Load(const char* model_name)
 {
-    BE_LOG("Loading Module: %s", file);
-    Assimp::Importer import;
-    const aiScene* scene = import.ReadFile(file, aiProcess_Triangulate | aiProcess_FlipUVs);
+    BE_LOG("Loading Model: %s", model_name);
+    YAML::Node model_data = YAML::LoadFile(model_name);
 
-    if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+    id = NodeExtractor<std::string>("id", model_data).c_str();
+    path = NodeExtractor<std::string>("path", model_data).c_str();
+
+    for (auto it = model_data["mesh"].begin(); it != model_data["mesh"].end(); ++it)
     {
-        BE_LOG("ERROR::ASSIMP::%c", import.GetErrorString());
+        if (it->first.as<std::string>()._Equal("id"))
+        {
+            mesh_ids.push_back(it->second.as<std::string>().c_str());
+        }
+    }
+
+    if (mesh_ids.size() == 0)
+    {
         return;
     }
-    const std::string path = file;
-    directory = path.substr(0, path.find_last_of('\\'));
-
-    BE_LOG("Scene Summary");
-    BE_LOG("Meshes: %d", scene->mNumMeshes);
-    BE_LOG("Materials: %d", scene->mNumMaterials);
-    BE_LOG("Textures: %d", scene->mNumTextures);
-    BE_LOG("Cameras: %d", scene->mNumCameras);
-    BE_LOG("Animations: %d", scene->mNumAnimations);
-    BE_LOG("Lights: %d", scene->mNumLights);
-
-    ProcessNode(scene->mRootNode, scene);
-}
-
-void Model::ProcessNode(aiNode* node, const aiScene* scene)
-{
-    // process all the node's meshes (if any)
-    for (unsigned int i = 0; i < node->mNumMeshes; i++)
+    for (auto id : mesh_ids)
     {
-        aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
-        meshes.push_back(ProcessMesh(mesh, scene));
-    }
-    // then do the same for each of its children
-    for (unsigned int i = 0; i < node->mNumChildren; i++)
-    {
-        ProcessNode(node->mChildren[i], scene);
+        meshes.push_back(new Mesh(id));
     }
 }
 
-Mesh Model::ProcessMesh(aiMesh* mesh, const aiScene* scene)
+Model::~Model()
 {
-    std::vector<Vertex> vertices;
-    std::vector<unsigned int> indices;
-    std::vector<Texture> textures;
-
-    vec minPoint;
-    vec maxPoint;
-    minPoint.x = mesh->mVertices[0].x;
-    minPoint.y = mesh->mVertices[0].y;
-    minPoint.z = mesh->mVertices[0].z;
-    maxPoint = minPoint;
-
-    // process vertex positions, normals and texture coordinates
-    for (unsigned int i = 0; i < mesh->mNumVertices; i++)
+    for (auto mesh : meshes)
     {
-        Vertex vertex;
-        float3 vector;
-
-        // Position
-        vector.x = mesh->mVertices[i].x;
-        vector.y = mesh->mVertices[i].y;
-        vector.z = mesh->mVertices[i].z;
-        vertex.position = vector;
-
-        // Bounding Box
-        minPoint.x = std::min(vector.x, minPoint.x);
-        minPoint.y = std::min(vector.y, minPoint.y);
-        minPoint.z = std::min(vector.z, minPoint.z);
-        maxPoint.x = std::max(vector.x, maxPoint.x);
-        maxPoint.y = std::max(vector.y, maxPoint.y);
-        maxPoint.z = std::max(vector.z, maxPoint.z);
-
-        // Normals
-        vector.x = mesh->mNormals[i].x;
-        vector.y = mesh->mNormals[i].y;
-        vector.z = mesh->mNormals[i].z;
-        vertex.normal = vector;
-
-        // Texture coords (Mesh couln'd have any)
-        if (mesh->mTextureCoords[0])
-        {
-            float2 vec;
-            vec.x = mesh->mTextureCoords[0][i].x;
-            vec.y = mesh->mTextureCoords[0][i].y;
-            vertex.tex_coords = vec;
-        }
-        else
-        {
-            vertex.tex_coords = float2(0.0f, 0.0f);
-        }
-        vertices.push_back(vertex);
+        delete mesh;
     }
-
-    // process indices
-    for (unsigned int i = 0; i < mesh->mNumFaces; i++)
-    {
-        aiFace face = mesh->mFaces[i];
-        for (unsigned int j = 0; j < face.mNumIndices; j++)
-            indices.push_back(face.mIndices[j]);
-    }
-
-    // process material
-    if (mesh->mMaterialIndex >= 0)
-    {
-        aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
-        // Diffuse
-        std::vector<Texture> diffuseMaps = LoadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
-        textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
-        // Specular
-        std::vector<Texture> specularMaps = LoadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
-        textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
-    }
-
-    return Mesh(vertices, indices, textures, minPoint, maxPoint);
-}
-
-std::vector<Texture> Model::LoadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName)
-{
-    std::vector<Texture> textures;
-    for (unsigned int i = 0; i < mat->GetTextureCount(type); i++)
-    {
-        aiString str;
-        mat->GetTexture(type, i, &str);
-        bool skip = false;
-
-        //Check if the texture has already loaded
-        for (unsigned int j = 0; j < textures_loaded.size(); j++)
-        {
-            if (std::strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
-            {
-                textures.push_back(textures_loaded[j]);
-                skip = true;
-                break;
-            }
-        }
-
-        if (skip)
-        {
-            continue;
-        }
-
-        // if texture hasn't been loaded already, load it
-        Texture texture;
-        const int texId = App->textures->Load(str.C_Str());
-        if (texId < 0)
-        {
-            BE_LOG("Cannot Load Texture: %s", str.C_Str());
-            continue;
-        }
-
-        texture.id = static_cast<unsigned>(texId);
-        texture.type = typeName;
-        texture.path = str.C_Str();
-        textures.push_back(texture);
-        textures_loaded.push_back(texture); // add to loaded textures
-    }
-
-    return textures;
 }
 
 void Model::Draw() const
 {
     for (unsigned int i = 0; i < meshes.size(); i++)
     {
-        meshes[i].Draw();
+        meshes[i]->Draw();
     }
 }
+
+template <typename T>
+T NodeExtractor(const std::string& key, YAML::Node& node)
+{
+    for (auto it = node.begin(); it != node.end(); ++it)
+    {
+        if (it->first.as<std::string>()._Equal(key))
+        {
+            return it->second.as<T>();
+        }
+    }
+}
+
--- Source/src/core/rendering/Model.h
+++ Source/src/core/rendering/Model.h
@@ -5,36 +5,20 @@
 #include <vector>
 #include <string>
 
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
-
 class Model
 {
 public:
-    Model() = default;
-    Model(const char* file);
-    ~Model() = default;
+    Model(const char* model_name);
+    Model(Model&) = default;
+    Model(Model&&) = default;
+    ~Model();
 
     void Draw() const;
 
-    [[nodiscard]] bool IsValid() const { return (!directory.empty() && meshes.size() != 0 && textures_loaded.size() != 0); }
-    [[nodiscard]] const std::vector<Mesh>& GetMeshes() const { return meshes; }
-    [[nodiscard]] const std::string& GetDirectory() const { return directory; }
-
-    [[nodiscard]] const float3& GetOrigin() const { return position; }
-    void SetOrigin(const float3& origin) { position = origin; }
-
-    void Load(const char* file);
-
 private:
-    // model data
-    std::vector<Mesh> meshes{};
-    std::string directory;
-    std::vector<Texture> textures_loaded{};
-    float3 position;
-
-    void ProcessNode(aiNode* node, const aiScene* scene);
-    Mesh ProcessMesh(aiMesh* mesh, const aiScene* scene);
-    std::vector<Texture> LoadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName);
+    const char* id;
+    const char* path;
+    std::vector<const char*> mesh_ids{};
+    std::vector<Mesh*> meshes{};
+    void Load(const char* file);
 };
